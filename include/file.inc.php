9fe65185e9031f1bb35b09f36d37e69814b5cb2c
vadim12rus <vadim.dolganof@gmail.com>
2016-07-01 10:57:52 +0300

    Update file.inc.php

--- include/file.inc.php
+++ include/file.inc.php
@@ -1,10 +1,10 @@
 <?php
     require_once('config.inc.php');
-	function is_valid_type($file)
-	{
-	    $valid_types = array("image/jpg","image/jpeg", "image/bmp", "image/gif", "image/png");	
+    function is_valid_type($file)
+    {
+        $valid_types = array("image/jpg","image/jpeg", "image/bmp", "image/gif", "image/png");	
         return in_array($file['type'], $valid_types);
-	}
+    }
     
     function uploadFileAvatar($avatar, $uploadAvatar)
     {
@@ -20,8 +20,8 @@
     function fileExists(&$TARGET_PATH, &$fileName, $imgPath)
     {
         if (file_exists($TARGET_PATH))
-	    {      
+	{      
             $fileName = date("YmdHi").$fileName;
             $TARGET_PATH = $imgPath.$fileName;
-	    }
+	}
     }
