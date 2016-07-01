f1ab76022f3d3715899c03783ade89fccaf52c8f
vadim12rus <vadim.dolganof@gmail.com>
2016-07-01 16:41:59 +0300

    Update config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,14 +1,11 @@
 <?php
     define('DB_HOST', "localhost");
-    define('DB_USER', "co35515_reshnew");
-    define('DB_PASS', "vadimiz12rus");
-    define('DB_NAME', "co35515_reshnew");
+w");
     define('ROOT_PATH', dirname( dirname(__FILE__) . "../"));
     define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
     define('TEMPLATE_DIR', ROOT_DIR."/template/");
     define('COMPILE_DIR', ROOT_DIR."/lib/templates_c/");
-    define('CLIENT_ID', '5447629');
-    define('CLIENT_SECRET', '3WEkvO8QCOqrFRmJeUUu');
+
     define('REDIRECT_URI', 'http://co35515.tmweb.ru/handler/vk_auth.php');
     define('URL_OAUTH_VK',  'http://oauth.vk.com/authorize');
     define('AMOUNT_DATA_ON_PAGE',  '7');
